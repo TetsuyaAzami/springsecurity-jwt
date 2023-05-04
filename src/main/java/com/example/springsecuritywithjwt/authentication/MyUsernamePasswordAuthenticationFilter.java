7ed77e481de1918441c81ba547b4005958523a6e
Tetsuya Azami <zqianjian35@gmail.com>
2023-05-04 03:01:59 +0000

    jwt認証機能の作成

--- src/main/java/com/example/springsecuritywithjwt/authentication/MyUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/springsecuritywithjwt/authentication/MyUsernamePasswordAuthenticationFilter.java
@@ -1,13 +1,16 @@
 package com.example.springsecuritywithjwt.authentication;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -23,10 +26,18 @@ public class MyUsernamePasswordAuthenticationFilter extends UsernamePasswordAuth
 
 		this.setAuthenticationSuccessHandler((request, response, authentication) -> {
 			//
+			MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
+
+			String jwtToken = JWT.create().withIssuer("test-issuer").withIssuedAt(Instant.now())
+					.withExpiresAt(Instant.now().plus(Duration.ofDays(60)))
+					.withClaim("username", userDetails.getPerson().getUsername())
+					.withClaim("role", authentication.getAuthorities().iterator().next().toString())
+					.sign(Algorithm.HMAC256(System.getenv("SECRET_KEY")));
+
+			response.addHeader("X-AUTH-TOKEN", "Bearer " + jwtToken);
 			response.setStatus(200);
-			MyUserDetails user = (MyUserDetails) SecurityContextHolder.getContext()
-					.getAuthentication().getPrincipal();
-			response.getWriter().write((new ObjectMapper()).writeValueAsString(user.getPerson()));
+			response.getWriter()
+					.write(new ObjectMapper().writeValueAsString(authentication.getName()));
 		});
 	}
 
--- src/main/java/com/example/springsecuritywithjwt/conf/MySecurityConfig.java
+++ src/main/java/com/example/springsecuritywithjwt/conf/MySecurityConfig.java
@@ -1,20 +1,47 @@
 package com.example.springsecuritywithjwt.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import com.example.springsecuritywithjwt.authentication.MyAuthenticationUserDetailsService;
+import com.example.springsecuritywithjwt.authentication.MyRequestHeaderAuthenticationFilter;
+import com.example.springsecuritywithjwt.authentication.MyUserDetailsService;
 import com.example.springsecuritywithjwt.authentication.MyUsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class MySecurityConfig {
+
+	@Autowired
+	public void configureProvider(AuthenticationManagerBuilder auth,
+			MyUserDetailsService myUserDetailsService,
+			MyAuthenticationUserDetailsService myAuthenticationUserDetailsService)
+			throws Exception {
+		//
+		PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider =
+				new PreAuthenticatedAuthenticationProvider();
+		preAuthenticatedAuthenticationProvider
+				.setPreAuthenticatedUserDetailsService(myAuthenticationUserDetailsService);
+		preAuthenticatedAuthenticationProvider
+				.setUserDetailsChecker(new AccountStatusUserDetailsChecker());
+		auth.authenticationProvider(preAuthenticatedAuthenticationProvider);
+
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(myUserDetailsService);
+		daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(8));
+		auth.authenticationProvider(daoAuthenticationProvider);
+	}
 
 	@Bean
 	public AuthenticationManager authenticationManager(
@@ -28,18 +55,16 @@ public class MySecurityConfig {
 		//
 		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/api/login").permitAll()
 				.anyRequest().authenticated());
+
 		AuthenticationManager authenticationManager =
 				authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+
 		http.addFilter(new MyUsernamePasswordAuthenticationFilter(authenticationManager));
+		http.addFilter(new MyRequestHeaderAuthenticationFilter(authenticationManager));
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.csrf().disable();
 
 		return http.build();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		//
-		return new BCryptPasswordEncoder(8);
 	}
 }
